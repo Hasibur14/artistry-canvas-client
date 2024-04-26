08a123de051c7e50e60b01ac1a474ad00678651b
hasibur14 <hasib141415@gmail.com>
2024-04-27 01:06:45 +0600

    added dark light theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 import title from '../assets/images/title.png';
@@ -6,16 +6,28 @@ import title from '../assets/images/title.png';
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
     const [isHovering, setIsHovering] = useState(false);
+    const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        document.querySelector('html').setAttribute('data-theme', theme);
+    }, [theme]);
 
     const handleSignOut = () => {
         logOut()
             .then()
-            .catch()
+            .catch();
     };
 
     const toggleHover = () => {
         setIsHovering(!isHovering);
     };
+
+    const handleToggle = (e) => {
+        const newTheme = e.target.checked ? 'synthwave' : 'light';
+        setTheme(newTheme);
+    };
+
 
     const links = (
         <>
@@ -42,7 +54,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="bg-neutral-100 fixed z-10 w-full shadow-md">
+        <div className={` fixed z-10 w-full shadow-md ${theme === 'synthwave' ? 'dark' : 'bg-neutral-100'}`}>
             <div className="navbar flex justify-between lg:mx-16">
                 <div className="navbar">
                     {/* Dropdown for mobile view */}
@@ -71,7 +83,7 @@ const Navbar = () => {
                     <div onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
                         {/* Conditionally render user image */}
                         {user && user.photoURL ? (
-                            <img className="w-12 rounded-full" src={user.photoURL} alt="User" />
+                            <img className="w-12 rounded-full border-[#F00]" src={user.photoURL} alt="User" />
                         ) : null}
 
                         {isHovering && user && (
@@ -84,18 +96,23 @@ const Navbar = () => {
                     {/* Conditional rendering based on user authentication */}
                     <div className="hidden lg:flex">
                         {user ? (
-                            <button onClick={handleSignOut} className="bg-[#F00] px-4 py-3 font-bold rounded-md text-lg hover:bg-transparent border-2 border-[#F00] text-white hover:text-[#F00]">LOG OUT</button>
+                            <button onClick={handleSignOut} className="bg-[#F00] px-3 py-3 font-bold rounded-md text-lg hover:bg-transparent border-2 border-[#F00] text-white hover:text-[#F00]">LOG OUT</button>
                         ) : (
                             <Link to='/login'>
-                                <button className="bg-[#F00] px-4 py-3 font-bold rounded-md text-lg hover:bg-transparent border-2 border-[#F00] text-white hover:text-[#F00]">LOG IN</button>
+                                <button className="bg-[#F00] px-3 py-3 font-bold rounded-md text-lg hover:bg-transparent border-2 border-[#F00] text-white hover:text-[#F00]">LOG IN</button>
                             </Link>
                         )}
                     </div>
                     <div className="hidden lg:flex">
                         <Link to='/register'>
-                            <button className="bg-transparent px-6 py-3 font-bold rounded-md text-lg text-[#F00] hover:bg-[#F00] border-2 border-[#F00] hover:text-white">Register</button>
+                            <button className="bg-transparent px-3 py-3 font-bold rounded-md text-lg text-[#F00] hover:bg-[#F00] border-2 border-[#F00] hover:text-white">Register</button>
                         </Link>
                     </div>
+                    <label className="cursor-pointer grid place-items-center">
+                        <input onChange={handleToggle} type="checkbox" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+                        <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                        <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </label>
                 </div>
             </div>
         </div>
