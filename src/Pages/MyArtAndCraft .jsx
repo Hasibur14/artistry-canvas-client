356cefafb13848a292f6bffe14aecccc1d9b92ba
hasibur14 <hasib141415@gmail.com>
2024-04-28 18:32:34 +0600

    added my art and craft section

--- src/Pages/MyArtAndCraft .jsx
+++ src/Pages/MyArtAndCraft .jsx
@@ -1,4 +1,5 @@
 import { AiOutlineDelete } from "react-icons/ai";
+import { FaRegEye } from "react-icons/fa6";
 import { MdOutlineBrowserUpdated } from "react-icons/md";
 import image from '../assets/images/banner-4.jpg';
 
@@ -7,49 +8,39 @@ import image from '../assets/images/banner-4.jpg';
 
 const MyArtAndCraft = () => {
     return (
-        <div className="container mx-auto mt-6 mb-16 grid grid-cols-1 lg:grid-cols-2 gap-6">
-            <section className=" dark:bg-gray-100 dark:text-gray-800 border rounded-lg">
-                <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="flex items-center justify-center p- mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                        <img src={image} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
-                    </div>
-                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-3xl font-bold leading-none ">
-                            Painting and Drawing
-                        </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12">Description</p>
-                        <p>Price: 1500</p>
-                        <p>Rating:  4.5</p>
-                        <p>customization: yes</p>
-                        <p>In stock</p>
-                        <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <a className="flex btn px-8 py-2 text-lg font-semibold rounded dark:bg-blue-600 dark:text-gray-50  bg-slate-900 text-white items-end text-center">  <MdOutlineBrowserUpdated /> Update</a>
-                            <a className="flex btn px-8 py-2 text-lg font-semibold border rounded dark:border-gray-800 bg-red-500 text-white items-center"><AiOutlineDelete />Delete</a>
+        <div className="container mx-auto mt-6 mb-16 ">
+
+
+            <select className="select select-error w-full max-w-xs mb-10">
+                <option disabled selected>Customization</option>
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+
+
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                <section className=" border rounded-lg">
+                    <div className="flex space-x-4 p-6">
+                        <div className="w-2/5">
+                            <img className="rounded-xl" src={image} alt="" />
                         </div>
-                    </div>
-                </div>
-            </section>
-            <section className=" dark:bg-gray-100 dark:text-gray-800  border rounded-lg">
-                <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                        <img src={image} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
-                    </div>
-                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-3xl font-bold leading-none ">
-                            Painting and Drawing
-                        </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12">Description</p>
-                        <p>Price: 1500</p>
-                        <p>Rating:  4.5</p>
-                        <p>customization: yes</p>
-                        <p>In stock</p>
-                        <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <a className="flex btn px-8 py-2 text-lg font-semibold rounded dark:bg-blue-600 dark:text-gray-50  bg-slate-900 text-white items-end text-center">  <MdOutlineBrowserUpdated /> Update</a>
-                            <a className="flex btn px-8 py-2 text-lg font-semibold border rounded dark:border-gray-800 bg-red-500 text-white items-center"><AiOutlineDelete />Delete</a>
+                        <div className="w-2/5 space-y-3">
+                            <h4 className="text-xl font-bold">Item</h4>
+                            <p> <span className="font-bold">Price: </span></p>
+                            <p> <span className="font-bold">Rating: </span></p>
+                            <p> <span className="font-bold">Customization:</span></p>
+                            <p> <span className="font-bold">Stock Status: </span></p>
                         </div>
+                        <div className="space-y-1 mt-6" >
+                            <button className='flex w-28 bg-[#D2B48C] text-white  px-4 py-2 rounded-t-lg'> <span className="text-xl mr-2"><FaRegEye /></span>View</button>
+                            <button className='flex w-28 bg-cyan-600 text-white  px-4 py-2 dark:bg-sky-500'> <span className="text-xl mr-2"><MdOutlineBrowserUpdated /></span>Update</button>
+                            <button className='flex w-28 bg-[#EA4744] text-white  px-4 py-2 rounded-b-lg'> <span className="text-xl mr-2"><AiOutlineDelete /></span>  Delete</button>
+                        </div>
+
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
     );
 };
